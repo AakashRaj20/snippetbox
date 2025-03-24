eef226cfd21e9b126efcfcda0c4041afbc6f97f1
Aakash Raj <aakashraj914@gmail.com>
2025-03-24 23:28:33 +0530

    completed till query string

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -23,11 +25,43 @@ func home(w http.ResponseWriter, r *http.Request){
 
 // Add a snippet view handler function 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	 // Extract the value of the id parameter from the query string and try to
+    // convert it to an integer using the strconv.Atoi() function. If it can't
+    // be converted to an integer, or the value is less than 1, we return a 404 page
+    // not found response.
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+    // and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "display a specific snipped with ID %d...", id)
 	w.Write([]byte("Display a specific snippet"))
 }
 
 // Add a SnippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r * http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+    
+        // If it's not, use the w.WriteHeader() method to send a 405 status
+        // code and the w.Write() method to write a "Method Not Allowed"
+        // response body. We then return from the function so that the
+        // subsequent code is not executed.
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+        // response header map. The first parameter is the header name, and
+        // the second parameter is the header value.
+
+		// Use the http.Error() function to send a 405 status code and "Method Not
+        // Allowed" string as the response body.
+	if r.Method != http.MethodPost{
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet...."))
 }
 
